c2b7cd2e9dda4654210d00bd61a14d760ac804fd
wongw859 <wongw859@gmail.com>
2020-11-02 17:41:30 +0800

    register page changed

--- public/js/register.js
+++ public/js/register.js
@@ -1,19 +1,30 @@
-//sidebar toggle
-const sidebarOpen = false;
-const sidebar = document.getElementById('sidebar')
-const sidebarCloseIcon = document.getElementById('sidebarIcon');
+// registration employees and employers view
+$(document).ready(function() {
+  const select = document.getElementById('mySelect')
+  // const employerOption = select.options[2];
+  let click = 0;
 
-function toggleSidebar(){
-  if(!sidebarOpen){
-    sidebar.classList.add('sidebar_responsive')
-    // sidebarCloseIcon.style.display = "inline";
-    sidebarOpen = true;
-  }
-}
-
-function closeSidebar(){
-  if(sidebarOpen){
-    sidebar.classList.remove('sidebar_responsive')
-    sidebarOpen = false;
-  }
-}
+  select.addEventListener('input', function(e) {
+    let optionTarget = e.target.value;
+    if (optionTarget == 'employer' && click === 0) {
+      $('.form :nth-child(7)').append(
+        `<div class='inputfield' id='businessRegNo'>
+        <label>Business Registration Number</label>
+        <input name="businessRegNo" type="text" class="input">
+        </div>
+        
+        <div class='inputfield' id='payment'>
+        <label>Do you want to upgrade to premium plan?</label>
+        <input name="payment" type="checkbox" class="input">
+        </div>
+        `
+        );
+        click++;
+        console.log(click);
+      } else {
+        click = 0;
+        $('#businessRegNo').remove()
+        $('#payment').remove();
+      }
+  })
+})
