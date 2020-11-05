71d48f8205ef2d40449651808c38635a47efb159
wongw859 <wongw859@gmail.com>
2020-11-05 10:07:47 +0800

    migrations

---
+++ migrations/20201105093521_new_tables.js
@@ -0,0 +1,32 @@
+//create job bookmark, employers_jobs listing ,  
+
+exports.up = function(knex) {
+  return knex.schema
+  .createTable('jobs_bookmarks', function(table){
+    table.increments('id').primary();
+    table.integer('jobs_id').unsigned();
+    table.foreign('jobs_id').references('jobs.id');
+    table.integer('employee_id').unsigned();
+    table.foreign('employee_id').references('employees.id')
+  })
+  .createTable('employers_job_listings', function(table){
+    table.increments('id').primary();
+    table.integer('employers_id').unsigned();
+    table.foreign('employers_id').references('employers.id');
+    table.integer('jobs_id').unsigned();
+    table.foreign('jobs_id').references('jobs.id');
+  })
+  .createTable('talent_bookmarks', function(table){
+    table.increments('id').primary();
+    table.integer('employers_id').unsigned();
+    table.foreign('employers_id').references('employers.id');
+    table.integer('employees_id').unsigned();
+    table.foreign('employees_id').references('employees.id');
+  })
+
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('jobs_bookmarks').dropTable('employers_job_listings').dropTable('talent_bookmarks');
+};
+
