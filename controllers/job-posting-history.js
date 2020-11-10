aee0e15a65d78831f897521ae54d32747fc54ac3
Venus Chan <vchan446@gmail.com>
2020-11-10 11:16:45 +0800

    working jobs posting and jp history

--- controllers/job-posting-history.js
+++ controllers/job-posting-history.js
@@ -2,21 +2,41 @@ const knex = require('../models/knex');
 
 
 //routes
-exports.getJobHist = async(req, res, next) => {
-        // console.log(req.user.id, 'this is user id')
-        let query = await knex.from('employers_job_listings').select('*').where('id',`${req.user.id}`);
-        let jobQueries = await knex('jobs').select('*').where('id',`${query.employers_id}`)
-        console.log(query.employers_id,'this is employers id');
-        console.log(jobQuery.company,'this is respective jobs');
+exports.getJobHist = async (req, res, next) => {
+    // console.log(req.user.id, 'this is user id')
+    let query = await knex.from('employers_job_listings').select().where('employers_id', `${req.user.id}`);
+    console.log(query, `FUCKKKKKKKK`)
 
-        res.render('users/job-posting-history', {
-            pageTitle: 'Job posting',
-            jobQueries: jobQueries 
-        })
-    
+    let jobsId = [];
+
+    for (let i = 0; i < query.length; i++) {
+        jobsId.push(query[i].jobs_id)
+    }
+
+    let jobQueries;
+
+    let jobQueriesArr = []
+
+    for (let j = 0; j < jobsId.length; j++) {
+        jobQueries = await knex('jobs').select().where('id', `${jobsId[j]}`)
+
+        jobQueriesArr.push(jobQueries[0])
+    }
+
+    console.log(jobQueriesArr)
+
+
+    // console.log(query.employers_id, 'this is employers id');
+    // console.log(jobQuery.company, 'this is respective jobs');
+
+    res.render('users/job-posting-history', {
+        pageTitle: 'Job posting',
+        jobQueries: jobQueriesArr
+    })
+
 }
 
-exports.postJobHist = async(req, res, next) => {
+exports.postJobHist = async (req, res, next) => {
 
 }
 
